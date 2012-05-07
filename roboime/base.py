38efed1f127121b13c5d1db9ab483f2477a2e959
Jan Segre <jan@segre.in>
2012-05-07 00:35:04 -0300

    More progres...

--- roboime/base.py
+++ roboime/base.py
@@ -1,40 +1,7 @@
 """
 This module holds the base classes.
 """
-from numpy import ndarray, isnan
-
-
-class Partciple(ndarray):
-    """Every object that has a position subclasses this class."""
-    
-    def __new__(cls, *args, **kwargs):
-        self = ndarray.__new__(cls, shape=(3))
-        self[0] = self[1] = self[2] = None
-        return self
-
-    @property
-    def x(self):
-        return self[0] if not isnan(self[0]) else None
-
-    @x.setter
-    def x(self, value):
-        self[0] = value
-
-    @property
-    def y(self):
-        return self[1] if not isnan(self[1]) else None
-
-    @y.setter
-    def y(self, value):
-        self[1] = value
-
-    @property
-    def z(self):
-        return self[2] if not isnan(self[2]) else None
-
-    @z.setter
-    def z(self, value):
-        self[2] = value
+from .utils import Particle
 
 
 class Component:
@@ -110,14 +77,6 @@ class Ball(Partciple):
         self.radius = None
 
 
-class Goal(Partciple):
-
-    def __init__(self):
-        self.width = None
-        self.depth = None
-        self.wall_width = None
-
-
 class Field:
     
     def __init__(self, right_team, left_team):
@@ -133,11 +92,13 @@ class Field:
         self.free_kick_distance = None
         self.penalty_spot_distance = None
         self.penalty_line_distance = None
+        self.goal_width = None
+        self.goal_depth = None
+        self.goal_wall_width = None
 
         # objects
-        self.right_goal = Goal()
         self.right_team = None
-        self.left_goal = Goal()
         self.left_team = None
+        self.referee = None
 
 
