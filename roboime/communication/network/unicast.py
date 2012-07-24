6bce9a8606fcff3702f8b11abc6474d8a6501dad
Jan Segre <jan@vialink.com.br>
2012-07-23 22:53:50 -0300

    Adding simple network unicast util.

---
+++ roboime/communication/network/unicast.py
@@ -0,0 +1,34 @@
+import socket
+
+
+class Unicast(object):
+
+    def __init__(self, address):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.address = address
+
+    def close(self):
+        self._sock.close()
+
+    @property
+    def host(self):
+        return self.address[0]
+
+    @group.setter
+    def host(self, value):
+        self.address = (value, self.port)
+
+    @property
+    def port(self):
+        return self.address[1]
+
+    @port.setter
+    def port(self, value):
+        self.address = (self.host, value)
+
+
+class UnicastSender(Unicast):
+
+    def send(self, data):
+        self._sock.sendto(data, self.address)
+
