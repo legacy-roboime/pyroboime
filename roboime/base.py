981953b26abe9900e1317b6e10c75486b652cd4c
Jan Segre <jan@vialink.com.br>
2012-07-25 08:25:27 -0300

    Adding Action class and some useful methods.

--- roboime/base.py
+++ roboime/base.py
@@ -19,7 +19,35 @@ class Component(object):
         return not self.working if self.working is not None else None
 
 
-class Robot(Partciple):
+class Action(Particle):
+    """An instance of this class determines what will a robot do."""
+
+    def __init__(self, x, y, angle, kick=0, chipkick=0, dribble=0):
+        self.robot = None
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.kick = kick
+        self.chipkick = chipkick
+        self.dribble = dribble
+
+    @property
+    def uid(self):
+        return self.robot.uid
+
+    @property
+    def speeds(self):
+        w = .1
+        from math import cos, sin
+        r = self.robot
+        a = r.angle
+        p = (self.x - r.x, self.y - r.y)
+        px = p[0] * cos(a) - p[1] * sin(a)
+        py = p[0] * sin(a) + p[1] * cos(a)
+        return map(lambda i: w * i, [px, py, self.angle - a])
+
+
+class Robot(Particle):
     """This class represents a robot, regardless of the team."""
 
     def __init__(self, uid, body=None, dribbler=None, kicker=None, wheels=[], battery=None):
@@ -35,6 +63,18 @@ class Robot(Partciple):
         self.kicker = kicker
         self.wheels = wheels
         self.battery = battery
+
+        # action to be dispatched by a commander
+        self._action = None
+
+    @property
+    def action(self):
+        return self._action
+
+    @action.setter
+    def action(self, value):
+        self._action = value
+        self._action.robot = self
 
     @property
     def height(self):
@@ -72,8 +112,38 @@ class Team(list):
         for r in self:
             r.team = self
 
+    def __getitem__(self, uid):
+        for r in self:
+            if r.uid == uid:
+                return r
+        else:
+            r = Robot(uid)
+            r.team = self
+            self.append(r)
+            return r
 
-class Ball(Partciple):
+    def __setitem__(self, *args):
+        #TODO: raise proper exception?
+        raise IndexError('__setitem__ not allowed')
+
+    @classmethod
+    def blue(cls, *args, **kwargs):
+        return cls(Blue, *args, **kwargs)
+
+    @classmethod
+    def yellow(cls, *args, **kwargs):
+        return cls(Yellow, *args, **kwargs)
+
+    @property
+    def is_blue(self):
+        return self.color == Blue
+
+    @property
+    def is_yellow(self):
+        return self.color == Yellow
+
+
+class Ball(Particle):
     """Well, a ball."""
 
     def __init__(self):
@@ -82,7 +152,7 @@ class Ball(Partciple):
 
 class Field(object):
 
-    def __init__(self, right_team, left_team):
+    def __init__(self, right_team=Team.yellow(), left_team=Team.blue()):
         # metric constants
         self.width = None
         self.length = None
@@ -100,10 +170,13 @@ class Field(object):
         self.goal_wall_width = None
 
         # objects
-        self.right_team = None
-        self.left_team = None
+        self.right_team = right_team
+        self.left_team = left_team
         self.referee = None
         self.ball = Ball()
+
+    def switch_sides(self):
+        self.right_team, self.left_team = self.left_team, self.right_team
 
     @property
     def yellow_team(self):
