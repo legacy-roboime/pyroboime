fd241da83438f9ac0a70fd5377c5336948a6bf45
Jan Segre <jan@vialink.com.br>
2012-07-29 22:27:05 -0300

    Adding math module to use degrees instead of radians.

---
+++ roboime/mathutils.py
@@ -0,0 +1,16 @@
+import math
+
+_trigonom_ = ['sin', 'cos', 'tan',]
+_invtrigonom_ = ['a' + f for f in _trigonom_] + ['atan2',]
+_restricted_ = ['trunc',]
+
+for fun in dir(math):
+    if fun in _restricted_:
+        pass
+    elif fun in _trigonom_:
+        exec '{0} = lambda x: math.{0}(math.radians(x))'.format(fun) in globals()
+    elif fun in _invtrigonom_:
+        exec '{0} = lambda x: math.degrees(math.{0}(x))'.format(fun) in globals()
+    else:
+        exec '{0} = math.{0}'.format(fun)
+
