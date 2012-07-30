82749f1768ad38dd1e28940a4e48a89865aca355
Jan Segre <jan@vialink.com.br>
2012-07-30 14:10:39 -0300

    Working test.

--- roboime/tests/basic.py
+++ roboime/tests/basic.py
@@ -5,20 +5,24 @@ from ..interface.commander import SimCommander
 from ..interface.updater import SimVisionUpdater
 
 #TODO: do real tests
+field = Field()
+updater = SimVisionUpdater(field)
+commander = SimCommander(field.blue_team)
 
-def basicloop(times, delay):
+def basicloop(times=100, delay=10):
+    """times: how many cicles?, delay: cicle time in ms."""
     #setup
-    field = Field()
-    updater = SimVisionUpdater(field)
-    commander = SimCommander(field.blue_team)
+    #field = Field()
 
     #test action
-    a = Action(2, 2, 2)
-    field.blue_team[2].action = a
+    a = field.blue_team[2].action
+    a.x = -2.5
+    a.y = 1.5
+    a.angle = 30.0
 
     #the loop
     for _ in xrange(times):
         updater.step()
+        sleep(delay / 1000.0)
         commander.step()
-        sleep(delay)
 
