1a0c7725a6010f0540b4ba948b6c544e19332a96
Jan Segre <jan@segre.in>
2013-05-24 15:31:49 -0300

    Rename simple client example to voidclient.

--- sampleclient2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python -O
-from roboime.clients import simple
-
-if __name__ == '__main__':
-    app = simple.Simple(show_fps=False)
-    app.mainloop()
-else:
-    raise Exception('This is not a module.')
-
---
+++ voidclient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python -O
+from sys import argv
+from roboime.clients import simple
+
+if __name__ == '__main__':
+    show_fps = False
+    if len(argv) > 1:
+        if argv[1] == '--fps':
+            show_fps = True
+    app = simple.Simple(show_fps=show_fps)
+    app.mainloop()
+else:
+    raise Exception('This is not a module.')
+
