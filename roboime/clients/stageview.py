e0d0771d5423b008533fa2d695767d65ec9b1565
Jan Segre <jan@segre.in>
2013-05-29 15:17:46 -0300

    Ported field drawing.

--- roboime/clients/stageview.py
+++ roboime/clients/stageview.py
@@ -1,6 +1,6 @@
-from PyQt4.QtGui import QGraphicsItem, QGraphicsView, QColor, QBrush, QPainter, QGraphicsScene
+from PyQt4.QtGui import QGraphicsItem, QGraphicsView, QColor, QBrush, QPainter, QGraphicsScene, QPainterPath
 from PyQt4.QtCore import QRectF, Qt
-#from ..utils.mathutils import sin, cos
+from ..utils.mathutils import sin, cos
 
 # some known uuids
 BALL = 0xba11
@@ -15,7 +15,13 @@ WHITE = Qt.white
 ORANGE = QColor(0xff, 0xbb, 0x00)
 
 # some settings
-BORDER = 10
+BORDER = 200.0
+
+
+def scale(meters):
+    """to milimiters"""
+    return meters * 1e3
+s = scale
 
 
 class RobotItem(QGraphicsItem):
@@ -54,6 +60,17 @@ class RobotItem(QGraphicsItem):
         #painter.drawText(0, 0, "Teste")
 
 
+def draw_arc(x, y, radius_in, radius_out, angle_init, angle_end, painter):
+    path = QPainterPath()
+
+    path.moveTo(x + radius_in * cos(angle_init), y + radius_in * sin(angle_init))
+    path.arcTo(x - radius_out, y - radius_out, 2 * radius_out, 2 * radius_out, angle_init, angle_end - angle_init)
+    path.arcTo(x - radius_in, y - radius_in, 2 * radius_in, 2 * radius_in, angle_end, angle_init - angle_end)
+    path.closeSubpath()
+
+    painter.drawPath(path)
+
+
 class FieldItem(QGraphicsItem):
 
     def __init__(self, world):
@@ -65,13 +82,68 @@ class FieldItem(QGraphicsItem):
         return FIELD
 
     def boundingRect(self):
-        w = self.world
-        return QRectF(-w.length / 2, -w.width / 2, w.length, w.width)
+        width, height = s(self.world.length), s(self.world.width)
+        return QRectF(-1.5 * width, -1.5 * height, 3 * width, 3 * height);
 
     def paint(self, painter, option, widget):
+        width, height = s(self.world.length), s(self.world.width)
+        line = s(self.world.line_width)
+
+        # Save transformation:
+        old_transformation = painter.worldTransform()
+
+        # Change position
+        painter.translate(-width / 2, -height / 2)
+
+        painter.setBrush(WHITE)
         painter.setPen(WHITE)
 
-        painter.drawRect(self.boundingRect())
+        # Center
+        draw_arc(width / 2, height / 2, 0, 1.5 * line, 0, 360, painter)
+
+        # Sides
+        painter.drawRect(0.0, 0.0, line, height)
+        painter.drawRect(width - line, 0.0, line, height)
+        painter.drawRect(0.0, 0.0, width, line)
+        painter.drawRect(0.0, height - line, width, line)
+
+        # Central lines
+        radius = s(self.world.center_radius)
+        painter.fillRect((width - line) / 2, 0, line, height, WHITE)
+        draw_arc(width / 2, height / 2, radius - line, radius, 0, 360, painter)
+
+        # Defense lines
+        dradius = s(self.world.defense_radius)
+        dstretch = s(self.world.defense_stretch)
+
+        painter.drawRect(dradius - line, (height - dstretch) / 2, line, dstretch)
+        draw_arc(0, (height - dstretch) / 2, dradius - line, dradius, 0, 90, painter)
+        draw_arc(0, (height + dstretch) / 2, dradius - line, dradius, 270, 360, painter)
+
+        painter.drawRect(width - dradius, (height - dstretch) / 2, line, dstretch)
+        draw_arc(width, (height - dstretch) / 2, dradius - line, dradius, 90, 180, painter)
+        draw_arc(width, (height + dstretch) / 2, dradius - line, dradius, 180, 270, painter)
+
+        # Penalty
+        penalty_line = s(self.world.penalty_line_distance)
+        draw_arc(penalty_line, height / 2, 0, line, 0, 360, painter)
+        draw_arc(width - penalty_line, height / 2, 0, line, 0, 360, painter)
+
+        # Goals
+        gwidth = s(self.world.goal_width)
+        gdepth = s(self.world.goal_depth)
+        gline = s(self.world.goal_wall_width)
+
+        painter.drawRect(-gdepth - gline, (height - gwidth) / 2 - gline, gdepth + gline, gline)
+        painter.drawRect(-gdepth - gline, (height - gwidth) / 2, gline, gwidth)
+        painter.drawRect(-gdepth - gline, (height + gwidth) / 2, gdepth + gline, gline)
+
+        painter.drawRect(width, (height - gwidth) / 2 - gline, gdepth + gline, gline)
+        painter.drawRect(width + gdepth, (height - gwidth) / 2, gline, gwidth)
+        painter.drawRect(width, (height + gwidth) / 2, gdepth + gline, gline)
+
+        # Reset transformation
+        painter.setTransform(old_transformation)
 
 
 class BallItem(QGraphicsItem):
@@ -106,14 +178,35 @@ class StageView(QGraphicsView):
         self.setRenderHints(QPainter.Antialiasing)
         self.setRenderHints(QPainter.SmoothPixmapTransform)
         self.setScene(QGraphicsScene(0, 0, 0, 0))
+        self._world = None
+
+    @property
+    def world(self):
+        return self._world
+
+    @world.setter
+    def world(self, w):
+        self._world = w
+        width, height = s(w.length), s(w.width)
+        self.setScene(QGraphicsScene(-width / 2, -height / 2, width, height))
+        #self.setScene(QGraphicsScene(-w.width / 2, -w.length / 2, w.width, w.length))
+        #self.scale(1, -1)
 
     def redraw(self):
+        # TODO: only do this when geometry changes
+        self.scene().clear()
+        self.world = self.world
+
+        # TODO this seems bad, something more performatic is desired
         scene = self.scene()
         scene.clear()
         with self.world as w:
             field = FieldItem(w)
             scene.addItem(field)
-            field.setPos(-w.length / 2, -w.width / 2)
+            width, height = s(self.world.length), s(self.world.width)
+            #field.setPos(-width / 2, -height / 2)
+            #field.setPos(BORDER / 2, BORDER / 2)
+            field.setPos(0, 0)
 
             #self.setScene(QGraphicsScene(-w.width / 2, -w.length / 2, w.width, w.length))
             #scene = self.scene()
@@ -136,4 +229,4 @@ class StageView(QGraphicsView):
             #    self.scene().addItem(ri)
             #    ri.setPos(r.x, r.y)
 
-            self.fitInView(-w.length / 2 - BORDER, -w.width / 2 - BORDER, w.width + 2 * BORDER, w.length + 2 * BORDER, Qt.KeepAspectRatio)
+            self.fitInView(-width / 2 - BORDER, -height / 2 - BORDER, width + 2 * BORDER, height + 2 * BORDER, Qt.KeepAspectRatio)
