04304d7ebc039ad1da22fb4cdb8aaf7194de35f1
Jan Segre <jan@segre.in>
2013-05-29 18:04:23 -0300

    Using plays for sample.

--- roboime/clients/qtgraphical.py
+++ roboime/clients/qtgraphical.py
@@ -15,7 +15,9 @@ from ..interface import SimulationInterface
 #from ..core.skills import sampleddribble
 #from ..core.skills import sampledkick
 #from ..core.skills import followandcover
-from ..core.skills import sampledchipkick
+#from ..core.skills import sampledchipkick
+#from ..core.plays import autoretaliate
+from ..core.plays import stop
 
 
 class GraphicalWorld(World, QtCore.QMutex):
@@ -88,21 +90,29 @@ class Intelligence(QtCore.QThread):
         self.world = world
         self.skill = None
         self.interface = SimulationInterface(self.world)
+        self.plays = {
+            #'autoretal_b': autoretaliate.AutoRetaliate(world.blue_team, 0),
+            #'autoretal_y': autoretaliate.AutoRetaliate(world.yellow_team, 0),
+            'stop_b': stop.Stop(world.blue_team, 0),
+            'stop_y': stop.Stop(world.yellow_team, 0),
+        }
 
     def _loop(self):
-        if 2 in self.world.blue_team:
-            r = self.world.blue_team[2]
-            r.max_speed = 2.0
-            if self.skill is None:
-                self.skill = sampledchipkick.SampledChipKick(r, lookpoint=self.world.left_goal)
-                #self.skill = followandcover.FollowAndCover(r, follow=self.world.ball, cover=self.world.blue_team[3])
-                #self.skill = sampledkick.SampledKick(r, lookpoint=self.world.left_goal)
-                #self.skill = sampleddribble.SampledDribble(r, lookpoint=self.world.left_goal)
-                #self.skill = drivetoball.DriveToBall(r, lookpoint=self.world.left_goal)
-                #self.skill = gotoavoid.GotoAvoid(r, target=Point(0, 0), avoid=self.world.ball)
-                #self.skill = goto.Goto(r, target=Point(0, 0))
-                #self.skill = goto.Goto(r, x=r.x, y=r.y, angle=90, speed=1, ang_speed=10)
-            self.skill.step()
+        #if 2 in self.world.blue_team:
+        #    r = self.world.blue_team[2]
+        #    r.max_speed = 2.0
+        #    if self.skill is None:
+        #        self.skill = sampledchipkick.SampledChipKick(r, lookpoint=self.world.left_goal)
+        #        #self.skill = followandcover.FollowAndCover(r, follow=self.world.ball, cover=self.world.blue_team[3])
+        #        #self.skill = sampledkick.SampledKick(r, lookpoint=self.world.left_goal)
+        #        #self.skill = sampleddribble.SampledDribble(r, lookpoint=self.world.left_goal)
+        #        #self.skill = drivetoball.DriveToBall(r, lookpoint=self.world.left_goal)
+        #        #self.skill = gotoavoid.GotoAvoid(r, target=Point(0, 0), avoid=self.world.ball)
+        #        #self.skill = goto.Goto(r, target=Point(0, 0))
+        #        #self.skill = goto.Goto(r, x=r.x, y=r.y, angle=90, speed=1, ang_speed=10)
+        #    self.skill.step()
+        for play in self.plays.itervalues():
+            play.step()
 
         with self.world:
             self.interface.step()
