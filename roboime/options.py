1b586da1e006f83b27163e8f0fed390ed897c44f
Ronan Paixão <ronanpaixao@gmail.com>
2013-06-15 19:41:37 -0300

    Update and position loggers

---
+++ roboime/options.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+General options during execution
+"""
+
+#Position Log filename. Use None to disable.
+#position_log_filename = "math/pos_log.txt"
+position_log_filename = None
