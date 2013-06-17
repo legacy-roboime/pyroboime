8cc72ed58cf73b1add1acf971bf7bf213a33d097
Victor Horta <victorhorta@gmail.com>
2013-06-17 00:56:21 -0300

    Fixed: Absence of joysticks was avoiding the client initialization.

--- roboime/core/tactics/joystick.py
+++ roboime/core/tactics/joystick.py
@@ -21,19 +21,21 @@ class Joystick(Tactic):
     def __init__(self, robot, **kwargs):
         super(Joystick, self).__init__(robot, deterministic=True, **kwargs)
         if pygame.joystick.get_count() == 0:
-            raise RuntimeError('No joysticks found.')
-        self.joystick = pygame.joystick.Joystick(0)
-        self.joystick.init()
-        self.fishingrod = FishingRod(robot, b_angle = 0)
+            # raise RuntimeError('No joysticks found.')
+            print 'WARNING: No joysticks found.'
+        else:    
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            self.fishingrod = FishingRod(robot, b_angle = 0)
 
-        self.chipkick_button = 3
-        self.kick_button = 0
-        self.dribble_button = 2
-        self.straffe_button = 1
+            self.chipkick_button = 3
+            self.kick_button = 0
+            self.dribble_button = 2
+            self.straffe_button = 1
 
-        self.normal_axis = 1
-        self.aux_axis = 0
-        self.power_axis = 2
+            self.normal_axis = 1
+            self.aux_axis = 0
+            self.power_axis = 2
         
 
     def _step(self):
@@ -45,27 +47,31 @@ class Joystick(Tactic):
             #    print("Joystick button pressed.")
             #if event.type == pygame.JOYBUTTONUP:
             #    print("Joystick button released.")
-        x = self.joystick.get_axis(self.aux_axis)
-        y = -self.joystick.get_axis(self.normal_axis)
-        power = (1 - self.joystick.get_axis(self.power_axis)) / 2
- 
-        if self.joystick.get_button(self.kick_button):
-            self.robot.action.kick = power
-        elif self.joystick.get_button(self.chipkick_button):
-            self.robot.action.chipkick = power
-        elif self.joystick.get_button(self.dribble_button):
-            self.robot.action.dribble = power
-
-        if self.joystick.get_button(self.straffe_button):
-            self.robot.action.speeds = y * self.speed_ratio, -x * self.speed_ratio, 0.0
+        if pygame.joystick.get_count() == 0:
+            print 'WARNING: No joysticks found.'
+            self.robot.action.speeds = 0.0, 0.0, 90 * self.speed_ratio
         else:
-            self.robot.action.speeds = y * self.speed_ratio, 0.0, -x * self.angle_ratio
-        
-        #angle = atan2(y, x)
-        #rod = array((x, y))
+            x = self.joystick.get_axis(self.aux_axis)
+            y = -self.joystick.get_axis(self.normal_axis)
+            power = (1 - self.joystick.get_axis(self.power_axis)) / 2
+     
+            if self.joystick.get_button(self.kick_button):
+                self.robot.action.kick = power
+            elif self.joystick.get_button(self.chipkick_button):
+                self.robot.action.chipkick = power
+            elif self.joystick.get_button(self.dribble_button):
+                self.robot.action.dribble = power
 
-        #self.fishingrod.threshold = norm(rod) * self.speed_ratio
-        #self.fishingrod.b_angle = angle
-        #self.fishingrod.t_angle = angle
-        #self.fishingrod.angle = angle
-        #self.fishingrod.step()
+            if self.joystick.get_button(self.straffe_button):
+                self.robot.action.speeds = y * self.speed_ratio, -x * self.speed_ratio, 0.0
+            else:
+                self.robot.action.speeds = y * self.speed_ratio, 0.0, -x * self.angle_ratio
+        
+            #angle = atan2(y, x)
+            #rod = array((x, y))
+
+            #self.fishingrod.threshold = norm(rod) * self.speed_ratio
+            #self.fishingrod.b_angle = angle
+            #self.fishingrod.t_angle = angle
+            #self.fishingrod.angle = angle
+            #self.fishingrod.step()
