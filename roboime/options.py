81f7b2bdd0e916a001b954ce9bac24ebe4600150
Ronan Paixão <ronanpaixao@gmail.com>
2013-06-17 21:36:14 -0300

    Add Q (generic) and R (3 values) to get more precise Kalman results

--- roboime/options.py
+++ roboime/options.py
@@ -7,15 +7,22 @@ General options during execution
 position_log_filename = "math/pos_log.txt"
 #position_log_filename = None
 
-#Position Log with Noise filename. Use None to disable.
-position_log_noise_filename = "math/pos_log_noise.txt"
-#position_log_filename = None
-
 #Command and Update Log filename. Use None to disable.
 cmdupd_filename = "math/commands.txt"
 #cmdupd_filename = None
 
 #Gaussian noise addition variances
-noise_var_x = 3.
-noise_var_y = 3.
-noise_var_angle = 0.05
+noise_var_x = 3.E-5
+noise_var_y = 3.E-5
+noise_var_angle = 1.
+
+# Process error estimate. The lower (higher negative exponent), more the filter
+# becomes like a Low-Pass Filter (higher confidence in the model prediction).
+Q = 1e-5
+
+# Measurement error variances (for the R matrix).
+# The higher (lower negative exponent), more the filter becomes like a
+# Low-Pass Filter (higher possible measurement error).
+R_var_x = 3.E-5
+R_var_y = 3.E-5
+R_var_angle = 3
