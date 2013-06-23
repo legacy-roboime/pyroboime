c35f50cc65a579dfe2afb5fa2674638c3f9d02a3
Renan Gemignani <renan.gemignani@gmail.com>
2013-06-23 19:03:39 -0300

    Transmitter should work now. Prototypes for other gui functions added.

--- roboime/interface/__init__.py
+++ roboime/interface/__init__.py
@@ -129,14 +129,14 @@ class Interface(Process):
 
 class TxInterface(Interface):
 
-    def __init__(self, world, filters=[], transmission_ipaddr='127.0.0.1', transmission_port=9050, mapping_yellow=None, mapping_blue=None, **kwargs):
+    def __init__(self, world, filters=[], transmission_ipaddr='172.30.3.73', transmission_port=9050, mapping_yellow=None, mapping_blue=None, kick_mapping_yellow=None, kick_mapping_blue=None,**kwargs):
         super(TxInterface, self).__init__(
             world,
 
             updaters=[updater.RealVisionUpdater(), updater.RefereeUpdater()],
             commanders=[
-                commander.Tx2012Commander(world.blue_team, mapping_dict=mapping_blue, ipaddr=transmission_ipaddr, port=transmission_port, verbose=True),
-                commander.Tx2012Commander(world.yellow_team, mapping_dict=mapping_yellow, ipaddr=transmission_ipaddr, port=transmission_port, verbose=True)
+                commander.Tx2012Commander(world.blue_team, mapping_dict=mapping_blue, kicking_power_dict=kick_mapping_yellow, ipaddr=transmission_ipaddr, port=transmission_port, verbose=True),
+                commander.Tx2012Commander(world.yellow_team, mapping_dict=mapping_yellow, kicking_power_dict=kick_mapping_blue, ipaddr=transmission_ipaddr, port=transmission_port, verbose=True)
             ],
 
             filters=filters + [filter.LowPass(), filter.Speed(), filter.Scale()],
@@ -160,7 +160,7 @@ class SimulationInterface(Interface):
                 filter.Kalman(),
                 filter.Speed(3), # first speed used to predict speed for Kalman
                 #Noise should be enabled during simulation, to allow real noise simulation
-                filter.Noise(options.noise_var_x,options.noise_var_y,options.noise_var_angle),
+                #filter.Noise(options.noise_var_x,options.noise_var_y,options.noise_var_angle),
                 filter.RegisterPosition("input"),
                 filter.Scale(),
             ],
