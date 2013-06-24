0edd7931b56b56f338be7292c31724f0adfe98b8
Jan Segre <jan@segre.in>
2013-06-24 19:28:27 +0200

    Some simple refactoring.

--- roboime/interface/__init__.py
+++ roboime/interface/__init__.py
@@ -1,8 +1,3 @@
-#from sys import platform
-#if platform == 'win32':
-#    from multiprocessing.dummy import Process, Queue, Event, Lock
-#else:
-#    from multiprocessing import Process, Queue, Event, Lock
 from multiprocessing import Process, Event
 from . import updater
 from . import commander
@@ -132,7 +127,10 @@ class TxInterface(Interface):
     def __init__(self, world, filters=[], transmission_ipaddr='127.0.0.1', transmission_port=9050, mapping_yellow=None, mapping_blue=None, kick_mapping_yellow=None, kick_mapping_blue=None,**kwargs):
         super(TxInterface, self).__init__(
             world,
-            updaters=[updater.RealVisionUpdater(), updater.RefereeUpdater()],
+            updaters=[
+                updater.RealVisionUpdater(),
+                updater.RefereeUpdater(),
+            ],
             commanders=[
                 commander.Tx2012Commander(world.blue_team, mapping_dict=mapping_blue, ipaddr=transmission_ipaddr, kicking_power_dict=kick_mapping_blue, port=transmission_port),
                 commander.Tx2012Commander(world.yellow_team, mapping_dict=mapping_yellow, kicking_power_dict=kick_mapping_yellow, ipaddr=transmission_ipaddr, port=transmission_port),
@@ -152,8 +150,14 @@ class SimulationInterface(Interface):
     def __init__(self, world, filters=[], **kwargs):
         super(SimulationInterface, self).__init__(
             world,
-            updaters=[updater.SimVisionUpdater(), updater.RefereeUpdater()],
-            commanders=[commander.SimCommander(world.blue_team), commander.SimCommander(world.yellow_team)],
+            updaters=[
+                updater.SimVisionUpdater(),
+                updater.RefereeUpdater(),
+            ],
+            commanders=[
+                commander.SimCommander(world.blue_team),
+                commander.SimCommander(world.yellow_team),
+            ],
             filters=filters + [
                 #filter.PositionLog(options.position_log_filename), #should be last, to have all data available
                 filter.Acceleration(),
