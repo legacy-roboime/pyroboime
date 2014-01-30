5fbb53517825a11cd4ac08032ed02332b07950de
Jan Segre <jan@segre.in>
2014-01-30 10:48:35 -0300

    Demo client now chooses interface based on config.

--- roboime/clients/demo_versus.py
+++ roboime/clients/demo_versus.py
@@ -15,10 +15,11 @@ from time import time
 
 from ..base import World
 from ..interface import SimulationInterface
-#from ..interface import TxInterface
+from ..interface import TxInterface
 from ..core.tactics import goalkeeper
 from ..core.tactics import zickler43 as zickler
 from ..core.skills import joystick
+from ..config import config
 
 
 class Demo(object):
@@ -28,25 +29,27 @@ class Demo(object):
 
     def __init__(self):
         self.world = World()
-        self.interface = SimulationInterface(self.world)
-        #self.interface = TxInterface(self.world)
+        if config['interface']['default'] == 'sim':
+            self.interface = SimulationInterface(self.world)
+        elif config['interface']['default'] == 'tx':
+            self.interface = TxInterface(self.world)
         self.stuff = {}
 
     def loop(self):
         if 0 in self.world.blue_team and 'attacker' not in self.stuff:
-            r = self.world.blue_team[0]
+            r = self.world.blue_team[1]
             self.stuff['attacker'] = zickler.Zickler43(r)
 
         if 0 in self.world.yellow_team and 'joystick' not in self.stuff:
-            r = self.world.yellow_team[0]
+            r = self.world.yellow_team[1]
             self.stuff['joystick'] = joystick.Joystick(r)
 
         if 1 in self.world.blue_team and 'goalkeeper1' not in self.stuff:
-            r = self.world.blue_team[1]
+            r = self.world.blue_team[0]
             self.stuff['goalkeeper1'] = goalkeeper.Goalkeeper(r)
 
         if 1 in self.world.yellow_team and 'goalkeeper2' not in self.stuff:
-            r = self.world.yellow_team[1]
+            r = self.world.yellow_team[0]
             self.stuff['goalkeeper2'] = goalkeeper.Goalkeeper(r)
 
         for s in self.stuff.itervalues():
