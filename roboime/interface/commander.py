f96fe4784be6792a6f35327c88895d0871c6afd3
Jan Segre <jan@segre.in>
2014-02-23 04:17:51 -0300

    Allowing simCommander to send wheel speeds.

--- roboime/interface/commander.py
+++ roboime/interface/commander.py
@@ -20,7 +20,6 @@ from time import time
 from math import isnan
 
 from ..config import config
-from ..communication.network import unicast
 from ..communication import grsim
 from ..utils.mathutils import sin, cos
 from ..utils.keydefaultdict import keydefaultdict
@@ -28,16 +27,16 @@ from ..communication.rftransmission.vivatxrx import VIVATxRx
 
 class Commander(object):
 #class Commander(Process):
-    '''
+    """
     This class instantiates a process that receives the actions for the robots and
     dispatches its actions to the transmitter. As of now, this class does NOT
     implement potential field collision avoidance. This will be treated on a future
     release. When using this, an updater is needed to apply the updates that are coming
-    both to the main window and to the Commander. 
+    both to the main window and to the Commander.
 
     It is still being decided whether or
     not this is going to be a thread in and of itself running in the main window
-    '''
+    """
 
     def __init__(self, maxsize=15):
         #self.world = World()
@@ -75,7 +74,7 @@ class Commander(object):
         raise NotImplemented
 
 class Tx2013Commander(Commander):
-    '''
+    """
     Sends commands as byte arrays via RF to the robots. Since the robots' firmware IDs are not the same as
     the UIDs sent by the VisionUpdater, a dict mapping the UIDs to the firmware IDs should be provided.
     If it is not provided, we shall use the trivial mapping: x => x.
@@ -83,7 +82,8 @@ class Tx2013Commander(Commander):
     This commander uses a transmission protocol compatible with the RoboIME MK-2012/2013 architecture.
     The main difference from this one to the Tx2012Commander are that this one does not require
     the usage of a separate program to actually execute the radio transmission.
-    '''
+    """
+
     def __init__(self, team, mapping_dict=None, kicking_power_dict=None, verbose=False, **kwargs):
         super(Tx2013Commander, self).__init__(**kwargs)
         self.default_map = mapping_dict is None
@@ -165,164 +165,29 @@ class Tx2013Commander(Commander):
                         self.log('|'.join('{:03d}'.format(x) for x in packet))
                     self.sender.send(packet)
 
-class Tx2012Commander(Commander):
-    '''
-    Sends commands as strings via unicast UDP to the C# transmitter process that will dispatch commands
-    via RF to the robots. Since the robots' firmware IDs are not the same as the UIDs sent by the
-    VisionUpdater, a dict mapping the UIDs to the firmware IDs should be provided. If it is not provided,
-    we shall use the trivial mapping: x => x.
-
-    This commander uses a transmission protocol compatible with the RoboIME MK-2012 architecture.
-    This might be deprecated soon. Or not.
-    '''
-    def __init__(self, team, mapping_dict=None, kicking_power_dict=None, ipaddr='127.0.0.1', port=9050, verbose=False, **kwargs):
-        super(Tx2012Commander, self).__init__(**kwargs)
-        self.default_map = mapping_dict is None
-        self.mapping_dict = mapping_dict if mapping_dict is not None else keydefaultdict(lambda x: x)
-        self.kicking_power_dict = kicking_power_dict if kicking_power_dict is not None else defaultdict(lambda: 100)
-        self.team = team
-        self.verbose = verbose
-        self.sender = unicast.UnicastSender(address=(ipaddr, port))
-        #self.sock = so
-        # FIXME: These values should be on the robot prototype to allow for mixed-chassis teams. NOT HERE!
-
-        # RoboIME 2013
-        #self.wheel_angles = [
-        #    -60.,
-        #    +60.,
-        #    +135.,
-        #    -135.,
-        #]
-        # RoboIME 2013
-        self.wheel_angles = [
-            -45.0,
-            +45.0,
-            +120.0,
-            -120.0,
-        ]
-
-        # RoboIME 2012
-        #self.wheel_angles = [
-        #    -57.,
-        #    +57.,
-        #    +135.,
-        #    -135.,
-        #]
-        #self.wheel_radius = 28.9
-        #self.wheel_distance = 80.6
-
-        #Values in meters.
-        self.wheel_radius = 0.0289
-        self.wheel_distance = 0.0806
-
-    def omniwheel_speeds(self, vx, vy, va):
-        return [(vy * cos(a) - vx * sin(a) + va * self.wheel_distance) / self.wheel_radius for a in self.wheel_angles]
-
-    def send(self, actions):
-        actions_dict = defaultdict(lambda:['0', '0', '0', '0', '0', '0', '0'])
-        dirty = False
-        if len(actions) > 0:
-            for a in actions:
-                string_list = []
-                if not a:
-                    continue
-                dirty = True
-                vx, vy, va = a.speeds
-                # Convert va to angular speed.
-                va = va * pi / 180
-                if self.default_map or a.uid in self.mapping_dict:
-                    pass
-                    #string_list.append(str(self.mapping_dict[a.uid]))
-                else:
-                    continue
-
-                string_list.extend([str(i) for i in self.omniwheel_speeds(vx, vy, -va)])
-                string_list.append(str((a.dribble or 0.0)))
-                if a.kick > 0 and self.kicking_power_dict[a.uid] > 0:
-                    string_list.append(str((a.kick * 100 / self.kicking_power_dict[a.uid] or 0.0)))
-                    #string_list.append(str((a.kick or 0.0)))
-                    string_list.append('0')
-                elif a.chipkick > 0 and self.kicking_power_dict[a.uid] > 0:
-                    string_list.append('0')
-                    string_list.append(str((a.chipkick * 100 / self.kicking_power_dict[a.uid] or 0.0)))
-                    #string_list.append(str((a.chipkick or 0.0)))
-                else:
-                    string_list.append('0')
-                    string_list.append('0')
-
-                actions_dict[self.mapping_dict[a.uid]] = string_list
-                a.reset()
-            if dirty:
-                string_list = []
-                for i in xrange(6):
-                    string_list.extend(actions_dict[i])
-
-                packet = ' '.join(string_list)
-
-                if packet:
-                    if self.verbose:
-                        # print self.kicking_power_dict
-                        print packet
-                    self.sender.send(packet)
-
-
-class TxCommander(Commander):
-    """
-    Sends commands as strings via unicast UDP to the C# transmitter process that will dispatch commands
-    via RF to the robots. Since the robots' firmware IDs are not the same as the UIDs sent by the
-    VisionUpdater, a function mapping the UIDs to the firmware IDs should be provided. If it is not provided,
-    we shall use the trivial mapping: x => x.
-
-    This commander uses the transmission protocol for the RoboIME MK-2013 architecture.
-
-    Main differences are:
-
-    - Not passing wheel velocities directly, sending instead the triplet vx, vy, theta;
-    - Chip kick.
-    """
-    def __init__(self, team, mapping_dict=None, ipaddr='127.0.0.1', port=9050, verbose=False, **kwargs):
-        super(TxCommander, self).__init__(**kwargs)
-        self.default_map = mapping_dict is None
-        self.mapping_dict = mapping_dict if mapping_dict is not None else keydefaultdict(lambda x: x)
-        self.team = team
-        self.verbose = verbose
-        self.sender = unicast.UnicastSender(address=(ipaddr, port))
-
-    def send(self, actions):
-        string_list = []
-        if len(actions) > 0:
-            for a in actions:
-                if not a:
-                    continue
-                vx, vy, va = a.speeds
-
-                if self.default_map or a.uid in self.mapping_dict:
-                    string_list.append(str(self.mapping_dict[a.uid]))
-                else:
-                    continue
-
-                string_list.append(str((a.robot.angle or 0.0)))
-                string_list.append(str((vx or 0.0)))
-                string_list.append(str((vy or 0.0)))
-                string_list.append(str((va or 0.0)))
-                string_list.append(str((a.dribble or 0.0)))
-                string_list.append(str((a.kick or 0.0)))
-                string_list.append(str((a.chipkick or 0.0)))
-                a.reset()
-
-            packet = ' '.join(string_list)
-            if packet:
-                if self.verbose:
-                    print packet
-                self.sender.send(packet)
-
 
 class SimCommander(Commander):
 
-    def __init__(self, team, address, **kwargs):
+    def __init__(self, team, address, send_omni=False, **kwargs):
         super(SimCommander, self).__init__(**kwargs)
         self.team = team
         self.sender = grsim.grSimSender(address)
+        self.send_omni = send_omni
+
+        # FIXME: These values should be on the robot prototype to allow for mixed-chassis teams. NOT HERE!
+        self.wheel_angles = [
+            240.0,
+            315.0,
+            45.0,
+            120.0,
+        ]
+        self.wheel_distance = 0.0850
+        self.wheel_radius = 0.0289
+
+    def omniwheel_speeds(self, vx, vy, va):
+        if isnan(vx) or isnan(vy) or isnan(va):
+            return [0.0, 0.0, 0.0, 0.0]
+        return [(vy * cos(a) - vx * sin(a) + va * self.wheel_distance) / self.wheel_radius for a in self.wheel_angles]
 
     def send(self, actions):
         packet = self.sender.new_packet()
@@ -350,6 +215,13 @@ class SimCommander(Commander):
                 c.velangular = va * pi / 180
                 c.spinner = (a.dribble or 0.0) > 0
                 c.wheelsspeed = False
+                if self.send_omni:
+                    c.wheelsspeed = True
+                    speeds = self.omniwheel_speeds(vx, vy, va * pi / 180)
+                    c.wheel1 = speeds[0]
+                    c.wheel2 = speeds[1]
+                    c.wheel3 = speeds[2]
+                    c.wheel4 = speeds[3]
 
                 # reset action values
                 a.reset()
