306f5fc553c2ab8856af9b4edaacac7b0e6fd845
victorhorta <victorhorta@gmail.com>
2014-10-16 01:31:18 -0300

    Added SerialTxRx -- VIVATxRx hardcodedly disabled!

---
+++ roboime/communication/rftransmission/serialtxrx.py
@@ -0,0 +1,107 @@
+#
+# Copyright (C) 2014 RoboIME
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+import sys
+import glob
+import serial
+import time
+
+from . import Transmitter
+
+
+def serial_ports():
+    """Lists serial ports
+
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of available serial ports
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM' + str(i + 1) for i in range(256)]
+
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this is to exclude your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+class SerialTxRx(Transmitter):
+    """
+    This class implements a Serial trasmitter for the
+    brand new transmitter.
+    """
+
+    # XXX: do not send a new packet before this delay
+    #      this was made due to too many packets clogging
+    #      the usb transmitter
+    delay = 0.05
+
+
+    def __init__(self, address='/dev/tty.usbmodem1451', verbose=False):
+        super(SerialTxRx, self).__init__()
+        
+
+        try:
+            self.transmitter = serial.Serial(address)
+        except AttributeError:
+            self.transmitter = None
+        except ValueError:
+            self.transmitter = None
+        self.is_working = self.transmitter is not None
+        self.verbose = verbose
+        self.last_sent = time.time()
+
+    def send(self, array):
+        now = time.time()
+        if now - self.last_sent < self.delay:
+            # too soon
+            return 0
+        else:
+            self.last_sent = now
+
+        if self.verbose:
+            print self.is_busy
+        if (not self.is_busy) and self.is_working:
+            return self.transmitter.write(array)
+        else:
+            return -1
+
+    def receive(self):
+        if self.verbose:
+            print self.is_busy
+        #sizev =  self.transmitter.ctrl_transfer(5824, 4, 3, 0, 8)
+        
+        data = self.transmitter.read(32)
+        return data
+
+    @property
+    def is_busy(self):
+        if not self.is_working:
+            return False
+        busy = self.transmitter.inWaiting()
+        return busy > 0
