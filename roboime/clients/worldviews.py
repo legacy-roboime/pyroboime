432b77a4b71810114c3626658f048eca5cfe1ed8
Jan Segre <jan@segre.in>
2015-02-23 15:48:11 -0300

    Updating copyright year.

--- roboime/clients/worldviews.py
+++ roboime/clients/worldviews.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 RoboIME
+# Copyright (C) 2013-2015 RoboIME
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
